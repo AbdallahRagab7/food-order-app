1d62d3171f01c3024d96af369998f6f2ba2b38b7
Abdallah Ragab <a.ragab8882@gmail.com>
2024-02-05 02:29:55 +0200

    persist cart in localstorage

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useReducer } from 'react';
+import { createContext, useReducer , useEffect} from "react";
 
 const CartContext = createContext({
   items: [],
@@ -8,7 +8,7 @@ const CartContext = createContext({
 });
 
 function cartReducer(state, action) {
-  if (action.type === 'ADD_ITEM') {
+  if (action.type === "ADD_ITEM") {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.item.id
     );
@@ -29,7 +29,7 @@ function cartReducer(state, action) {
     return { ...state, items: updatedItems };
   }
 
-  if (action.type === 'REMOVE_ITEM') {
+  if (action.type === "REMOVE_ITEM") {
     const existingCartItemIndex = state.items.findIndex(
       (item) => item.id === action.id
     );
@@ -46,37 +46,41 @@ function cartReducer(state, action) {
       };
       updatedItems[existingCartItemIndex] = updatedItem;
     }
-
     return { ...state, items: updatedItems };
   }
 
-  if (action.type === 'CLEAR_CART') {
+  if (action.type === "CLEAR_CART") {
     return { ...state, items: [] };
   }
 
   return state;
 }
-
+ const cartFromLocalStorage = JSON.parse(localStorage.getItem("cart") || "[]")
 export function CartContextProvider({ children }) {
-  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: [] });
+  const [cart, dispatchCartAction] = useReducer(cartReducer, { items: cartFromLocalStorage });
+
+  useEffect(() => {
+    localStorage.setItem('cart', JSON.stringify(cart.items));
+  }, [cart.items]);
+
 
   function addItem(item) {
-    dispatchCartAction({ type: 'ADD_ITEM', item });
+    dispatchCartAction({ type: "ADD_ITEM", item });
   }
 
   function removeItem(id) {
-    dispatchCartAction({ type: 'REMOVE_ITEM', id });
+    dispatchCartAction({ type: "REMOVE_ITEM", id });
   }
 
   function clearCart() {
-    dispatchCartAction({ type: 'CLEAR_CART' });
+    dispatchCartAction({ type: "CLEAR_CART" });
   }
 
   const cartContext = {
     items: cart.items,
     addItem,
     removeItem,
-    clearCart
+    clearCart,
   };
 
   return (
