f576867284813e67eb2cb31058a527fb7c9d39d1
Abdallah Ragab <a.ragab8882@gmail.com>
2024-02-05 03:21:36 +0200

    pagination , animation

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -18,7 +18,7 @@ export default function MealItem({ meal }) {
   return (
     <>
       <li className="meal-item">
-        <article>
+        <article className="meal-item-article">  
           <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
           <div>
             <h3>{meal.name}</h3>
--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,18 +1,58 @@
-import MealItem from "./MealItem.jsx";
-import useHttp from "../hooks/useHttp.js";
-import Error from "./Error.jsx";
-
+import React, { useState } from "react";
+import MealItem from "./MealItem";
+import useHttp from "../hooks/useHttp";
+import Error from "./Error";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
 const requestConfig = {};
 
-export default function Meals() {
+const Meals = () => {
   const {
     data: loadedMeals,
     isLoading,
     error,
   } = useHttp("http://localhost:3000/meals", requestConfig, []);
+
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 6;
+
+  const totalPages = Math.ceil(loadedMeals.length / itemsPerPage);
+  const handlePageChange = (newPage) => {
+    setCurrentPage(newPage);
+  };
+
+  const renderPaginationButtons = () => {
+    const buttons = [];
+    for (let i = 1; i <= totalPages; i++) {
+      buttons.push(
+        <button
+          key={i}
+          onClick={() => handlePageChange(i)}
+          className={currentPage === i ? "active" : ""}
+        >
+          {i}
+        </button>
+      );
+    }
+    return buttons;
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const indexOfLastMeal = currentPage * itemsPerPage;
+  const indexOfFirstMeal = indexOfLastMeal - itemsPerPage;
+  const currentMeals = loadedMeals.slice(indexOfFirstMeal, indexOfLastMeal);
 
   if (isLoading) {
     return <p className="center">Fetching meals...</p>;
@@ -22,15 +62,24 @@ export default function Meals() {
     return <Error title="Failed to fetch meals" message={error} />;
   }
 
-  // if (!data) {
-  //   return <p>No meals found.</p>
-  // }
-
   return (
-    <ul id="meals">
-      {loadedMeals.map((meal) => (
-        <MealItem key={meal.id} meal={meal} />
-      ))}
+    <div>
+      <ul id="meals">
+        {currentMeals.map((meal) => (
+          <MealItem key={meal.id} meal={meal} />
+        ))}
+      </ul>
+
+      <div className="pagination-container">
+        <button onClick={handlePreviousPage} disabled={currentPage === 1}>
+          Previous
+        </button>
+        {renderPaginationButtons()}
+        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+          Next
+        </button>
+      </div>
+
       <ToastContainer
         position="top-left"
         autoClose={1500}
@@ -43,6 +92,8 @@ export default function Meals() {
         pauseOnHover
         theme="dark"
       />
-    </ul>
+    </div>
   );
-}
+};
+
+export default Meals;
